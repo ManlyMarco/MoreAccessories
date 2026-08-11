8c9ec79e3b1ae9758289398d9b29556f8e0364ad
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2026-08-11 03:37:51 +0200

    Refactor project structure; Update to sdk-style csproj with build props

--- src/Core.Common/Common/Data.cs
+++ src/Core.Common/Common/Data.cs
@@ -2,7 +2,7 @@
 {
     public partial class MoreAccessories
     {
-        public const string versionNum = "2.0.21.1";
+        public const string versionNum = Constants.Version;
         public const string GUID = "com.joan6694.illusionplugins.moreaccessories";
 
         public static MoreAccessories _self;
